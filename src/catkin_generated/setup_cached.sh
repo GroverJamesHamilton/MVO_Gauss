632c6e76e0627eb770d4963f37286455e6062c29
Alesm512 <alesm512@student.liu.se>
2022-07-19 00:06:37 +0200

    Added files

--- src/catkin_generated/setup_cached.sh
+++ src/catkin_generated/setup_cached.sh
@@ -7,9 +7,3 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/labbare/abb_ws/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/labbare/abb_ws/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/labbare/abb_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
-export PWD='/home/labbare/abb_ws/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/labbare/abb_ws/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/labbare/abb_ws/src:$ROS_PACKAGE_PATH"
