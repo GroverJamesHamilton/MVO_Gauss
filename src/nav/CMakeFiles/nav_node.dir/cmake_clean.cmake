bfda8f6f91e32c09df2f45788f694c386f5b888c
Ditt Namn <alesm512@student.liu.se>
2025-03-06 03:28:56 +0100

    Removed old version of cpp files

---
+++ src/nav/CMakeFiles/nav_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/labbare/abb_ws/devel/lib/nav/nav_node"
+  "/home/labbare/abb_ws/devel/lib/nav/nav_node.pdb"
+  "CMakeFiles/nav_node.dir/src/nav_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/nav_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
