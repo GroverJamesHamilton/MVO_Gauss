ca8ecbe08da2062f73827b1112e7e78bb3513f41
Ditt Namn <alesm512@student.liu.se>
2025-03-06 01:37:24 +0100

    Clean up ignored files

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/alesm512/MVO_Gauss/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/alesm512/MVO_Gauss/devel;/home/alesm512/ABB_New_Msc/devel;/opt/ros/noetic'.split(';') if '/home/alesm512/MVO_Gauss/devel;/home/alesm512/ABB_New_Msc/devel;/opt/ros/noetic' != '' else []
--- build/nav/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alesm512/MVO_Gauss/src/nav
-# Build directory: /home/alesm512/MVO_Gauss/build/nav
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
